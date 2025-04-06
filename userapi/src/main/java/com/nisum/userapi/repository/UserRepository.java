4451889d272a4601606e27c70e96e0c5895ac2e3
Daniel Sanchez <daniel.sanchez@evertecinc.com>
2025-04-06 00:37:56 -0400

    Adding methods to verify if an email is already registered on the database.

---
+++ userapi/src/main/java/com/nisum/userapi/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.nisum.userapi.repository;
+
+import com.nisum.userapi.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+    Optional<User> findUserByEmail(String email);
+
+}
